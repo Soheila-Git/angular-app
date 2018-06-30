0e3168ef591d75b58abf71a2271ed5a95824bb81
Soheila Akbarzadeh <soheila.akbarzadeh@gmail.com>
2018-06-29 20:05:36 -0700

    add material dashboard and wizard component
    Use the CLIs:
    
    ng add @angular/material@6.2.1
    
    ng g @angular/material:material-dashboard --name=my-dash
    
    ng g c wizard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyDashComponent } from './my-dash/my-dash.component';
+import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
+import { WizardComponent } from './wizard/wizard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyDashComponent,
+    WizardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
