42aef21d8c0a7207a7b27a06b36f857a9b75e233
Soheila Akbarzadeh <soheila.akbarzadeh@gmail.com>
2018-06-29 21:10:32 -0700

    move wizard component to my-share-lib library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyDashComponent } from './my-dash/my-dash.component';
 import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
-import { WizardComponent } from './wizard/wizard.component';
+import { WizardComponent } from 'projects/my-share-lib/src/lib/wizard/wizard.component';
 
 @NgModule({
   declarations: [
